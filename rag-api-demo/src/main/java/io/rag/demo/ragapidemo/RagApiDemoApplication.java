50ce01101a50663503345ea5833c8e40aa5f253c
김현오/백엔드팀/서비스개발부문 <ho.kim@haezoom.com>
2025-07-01 16:50:27 +0900

    init

---
+++ rag-api-demo/src/main/java/io/rag/demo/ragapidemo/RagApiDemoApplication.java
@@ -0,0 +1,13 @@
+package io.rag.demo.ragapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagApiDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RagApiDemoApplication.class, args);
+  }
+
+}
---
+++ rag-api-demo/src/test/java/io/rag/demo/ragapidemo/RagApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rag.demo.ragapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagApiDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
